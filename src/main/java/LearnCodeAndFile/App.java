36e672c54bc6970f3dab88b8b3d4c04f6f160532
yuanfeiasima <382810567@qq.com>
2016-04-29 15:11:50 +0800

    first commit this is project for note

---
+++ src/main/java/LearnCodeAndFile/App.java
@@ -0,0 +1,13 @@
+package LearnCodeAndFile;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
